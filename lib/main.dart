0d7ec9d26153fd0b01ee2ec6d6fbd8c29eb0c2b8
Prysla <Pryslasaved@icloud.com>
2021-11-25 15:31:46 +0000

    Main edited

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Psaved Delivery',
+      title: 'Psaved Dery',
       theme: ThemeData(
         primarySwatch: Colors.blueGrey,
       ),
